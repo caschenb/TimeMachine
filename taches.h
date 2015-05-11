ee89bbe14e2f3c6b7f4e7b0687ca5a4aaf128dcc
caschenb <celine.aschen@gmail.com>
2015-05-11 16:10:55 +0200

    Update taches.h
    relation d'héritage entre tache (classe abstraite), tache unitaire et tache composite
    composition entre tache composite et tache
    si j'ai bien compris le design pattern fabrique remplace le tachemanager?

--- taches.h
+++ taches.h
@@ -3,8 +3,9 @@
 #include <iostream>
 #include <string>
 //#include "timing.h"
-
+/* ****************************************************TACHES**************************************************** */
 class Tache {  //la classe est abstraite
+	private:
 	string titre;
 	Date date_disponibilite;
 	Date echeance;
@@ -22,4 +23,39 @@ public:
 	//exemple de fonction virtuelle virtual void afficher(std::ostream& f= std::cout) const =0;
 };
 
+class Tache_unitaire : public Tache {
+	private:
+	Duree duree; 
+	public:
+	Tache(const string& t, const Duree& dur, const Date& dispo, const Date& deadline): Tache(t, dispo, deadline), duree (dur) {}
+	Duree getDuree() const { return duree; }
+};
+
+class Tache_composite : public Tache {
+	private:
+	Tache** taches;
+	unsigned int nb;
+	unsigned int nbMax;
+	~Tache_composite(){
+    		for (unsigned int i=0; i<nb; i++) delete taches[i];
+    		delete[] taches;
+    		nb=0;
+    		nbMax=0;
+    		taches=0;
+		}
+	Tache(const string& t, const Duree& dur, const Date& dispo, const Date& deadline): Tache(t, dispo, deadline) {}
+	~Tache_composite() {
+};
+
+
+/* *********************************** FABRIQUE TACHES ******************************************** */
+
+class FabriqueTache {
+public:
+ virtual Tache_unitaire *createTache_unitaire() = 0;
+ virtual Tache_composite *createTache_composite() = 0;
+};
+
+
+}
 #endif
