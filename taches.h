254d664c5fd1cc942b8d4a15e5380ffd2da0e02c
caschenb <celine.aschen@gmail.com>
2015-05-16 18:21:30 +0200

    Update taches.h

--- taches.h
+++ taches.h
@@ -3,7 +3,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include "Z:\LO21\TD5\timing.h" //penser a le mettre à jour à chaque fois
+//#include "Z:\LO21\TD5\timing.h" //penser a le mettre à jour à chaque fois
+#include "C:\Users\celine H\Desktop\UTC\LO21\TD5\timing.h"
 
 /**************************************************** EXCEPTIONS ***************************************/
 
@@ -20,20 +21,17 @@ private:
 class Tache;
 class Tache_unitaire;
 class Tache_composite;
-//attention a penser a declarer tachemanager avant tache ou le contraire en cas d'erreur
 
 class TacheManager {
 private:
 	Tache** taches;
 	unsigned int nb;
 	unsigned int nbMax;
-	void addItem(Tache* t);
-    Tache* trouverTache(const std::string& id) const;
 	std::string file;
-	  //on doit bloquer toutes les possibilités de
-	  //création et de destruction de l'opérateur =>privé
-	//TacheManager(const TacheManager& um);
+	TacheManager(const TacheManager& um); //empeche la duplication
 	TacheManager& operator=(const TacheManager& um);
+    void addItem(Tache* t); //était en private dans le TD
+    Tache* trouverTache(const std::string& id) const; //on accede à cette fonction via getTache
 	struct Handler {
 	    TacheManager* instanceUnique;
 	    Handler(): instanceUnique(0) {}
@@ -41,21 +39,19 @@ private:
 	};
 	static Handler handler;
 public:
-	TacheManager();
+	TacheManager():taches(0),nb(0),nbMax(0){}
 	virtual ~TacheManager();
+	/* regler probleme entre 2 ajouter tache différent ou 1 qui reconnait tache composite unitaire, mais ne pas laisser les 2*/
 	Tache_composite& ajouterTache(const std::string& id, const std::string& t,const TIME::Date& dispo, const TIME::Date& deadline);
-	Tache_unitaire& ajouterTache(const std::string& id, const std::string& t, const TIME::Duree& dur, const TIME::Date& dispo, const TIME::Date& deadline);
+	Tache_unitaire& ajouterTache(const std::string& id, const std::string& t, const TIME::Duree& dur, const TIME::Date& dispo, const TIME::Date& deadline, bool preempt);
     Tache_composite& ajouterTacheComposite(const std::string& id, const std::string& t, const TIME::Date& dispo, const TIME::Date& deadline);
-	void ajouterSousTache (const std::string& id_composite, const std::string& id_sous_tache);
-	friend class ProjetManager;
+	void ajouterSousTache (const std::string& id_composite, const std::string& id_sous_tache); //ajoute une tache à une tache composite
 	Tache& getTache(const std::string& id);
-	//Tache_composite& getTacheComposite(const std::string& id);
+    const Tache& getTache(const std::string& code) const;
     bool isTacheExistante(const std::string& id) const { return trouverTache(id)!=0; }
-	const Tache& getTache(const std::string& code) const;
-	void load(const std::string& f);
-	void save(const std::string& f);
-	//friend class Tache_composite;
 	void parcourirTacheComposite(const std::string&id);
+    void load(const std::string& f);
+	void save(const std::string& f);
 	static TacheManager& getInstance();
 	static void libererInstance();
 
@@ -109,13 +105,12 @@ private:
 	std::string titre;
 	TIME::Date date_disponibilite;
 	TIME::Date echeance;
-public:
+	bool in; /*permet de savoir si la tache est déjà dans une tache composite ou dans un projet, on prend l'hypothese qu'elle ne peut etre que dans une seule tache composite ou projet */
     Tache(const Tache& t);
     Tache& operator=(const Tache& t);
-	Tache(const std::string& id, const std::string& t, const TIME::Date& dispo, const TIME::Date& deadline):
-			identificateur(id), titre(t), date_disponibilite(dispo), echeance(deadline){}
-   // friend Tache& TacheManager::ajouterTache(const std::string& id, const std::string& t, const TIME::Date& dispo, const TIME::Date& deadline);
 public:
+	Tache(const std::string& id, const std::string& t, const TIME::Date& dispo, const TIME::Date& deadline):
+			identificateur(id), titre(t), date_disponibilite(dispo), echeance(deadline), in(0) {}
     virtual ~Tache() {};
     std::string getId() const { return identificateur; }
     void setId(const std::string& str);
@@ -126,50 +121,40 @@ public:
 	void setDatesDisponibiliteEcheance(const TIME::Date& disp, const TIME::Date& e) {
         if (e<disp) throw CalendarException("erreur Tache : date echeance < date disponibilite");
         date_disponibilite=disp; echeance=e;
-    }
+        }
     virtual void afficher(std::ostream& fout=std::cout) const=0;
+    bool isIn() const { return in; } //fonctions utilisées pour savoir si tache dans projet/tache composite
+    void setIn() { in=true; }
+    void setNotIn() { in=false; }
 };
 
-std::ostream& operator<<(std::ostream& f, const Tache& t);
+//std::ostream& operator<<(std::ostream& f, const Tache& t); inutile pour le moment
 
 class Tache_unitaire : public Tache {
 private:
 	TIME::Duree duree;
 	bool preemptive;
 public:
-	Tache_unitaire(const std::string& id, const std::string& t, const TIME::Duree& dur, const TIME::Date& dispo, const TIME::Date& deadline, bool preempt=false ): Tache(id, t, dispo, deadline), duree (dur), preemptive(preempt) {}
+	Tache_unitaire(const std::string& id, const std::string& t, const TIME::Duree& dur, const TIME::Date& dispo, const TIME::Date& deadline, bool preempt=false ): Tache(id, t, dispo, deadline), duree (dur), preemptive(preempt) {
+	if (preempt && dur.getDureeEnMinutes()>60*12) throw CalendarException("duree tache trop grande!");} //une tache preemptive ne peut avoir une durée >12h
 	TIME::Duree getDuree() const { return duree; }
 	bool isPreemptive() const { return preemptive; }
     void setPreemptive() { preemptive=true; }
     void setNonPreemptive() { preemptive=false; }
-   // friend Tache_unitaire& TacheManager::ajouterTache(const std::string& id, const std::string& t, const TIME::Duree& dur, const TIME::Date& dispo, const TIME::Date& deadline);
     void afficher(std::ostream& fout) const;
 };
 
-std::ostream& operator<<(std::ostream& fout, const Tache_unitaire& t);
+//std::ostream& operator<<(std::ostream& fout, const Tache_unitaire& t); inutile pour le moment
 
 class Tache_composite : public Tache {
-
 private:
-//protected:
 	typedef std::vector<Tache*> contTache;
 	contTache taches_composees;
-        //recopie et affectation interdite???
-     //   friend Tache& TacheManager::ajouterTache(const std::string& id, const std::string& t,const TIME::Date& dispo, const TIME::Date& deadline);
-
 public:
-  /*  Tache_composite& ajouterTacheComp(const TacheManager& m,const std::string& id, const std::string& t, const TIME::Date& dispo, const TIME::Date& deadline){
-	    m.TacheManager::ajouterTache(id,t,dispo,deadline);
-        taches_composees.push_back(&m);
-       return *this;
-    }*/
-    friend class TacheManager;
-    Tache_composite& ajouterTacheComp(Tache& t ){
-        taches_composees.push_back(&t);
-        return *this;
-        }
-	virtual ~Tache_composite(){} //a faire
-	Tache_composite(const std::string& id, const std::string& t, const TIME::Date& dispo, const TIME::Date& deadline): Tache(id, t, dispo, deadline) {
+   friend class TacheManager; //on utilise dans tache manager l'attribut taches_composees
+    Tache_composite& ajouterTacheComp(Tache& t ); //ajoute la tache dans le tableau de pointeurs
+	~Tache_composite(); //a faire
+	Tache_composite(const std::string& id, const std::string& t, const TIME::Date& dispo, const TIME::Date& deadline): Tache(id, t, dispo, deadline) { //definie la tache et reserve de la place pour le tableau de pointeurs
 	    taches_composees.reserve(10);
 	    }
     void afficher(std::ostream& fout= std::cout) const {
@@ -178,50 +163,42 @@ public:
 };
 
 /******************************************* PROJET MANAGER **********************************************/
+
 class Projet;
-class ProjetManager {
+
+class ProjetManager {  //a mettre en singleton
 private:
 	Projet** projets;
 	unsigned int nb;
 	unsigned int nbMax;
 	void addItem(Projet* t);
-   // Tache* trouverTache(const std::string& id) const;
-	//std::string file;
-	  //on doit bloquer toutes les possibilités de
-	  //création et de destruction de l'opérateur =>privé
-	//TacheManager(const TacheManager& um);
-	//TacheManager& operator=(const TacheManager& um);
+	//constructeur de recopie et operateur d'affectation uniquement déclarés car (futur) singleton
+    ProjetManager(const ProjetManager& p);
+    ProjetManager& operator=(const ProjetManager& p);
+    Projet* trouverProjet(const std::string& id) const; //on y a acces via getProjet
 public:
 	ProjetManager(): projets(0), nb(0), nbMax(0) {}
 	virtual ~ProjetManager();
-	//Tache& getTache(const std::string& id);
-	//Tache_composite& getTacheComposite(const std::string& id);
-//  friend class TacheManager;
     void ajouterTacheProjet( TacheManager& m, const std::string& id_projet, const std::string& id_tache);
     void parcourirTacheProjet(const std::string&id);
-    Projet& getProjet(const std::string& id);
-    Projet* trouverProjet(const std::string& id) const;
+    Projet& getProjet(const std::string& id); //vérifie que le projet existe avant d'appeler trouverProjet
     Projet& ajouterProjet(const std::string& id, const std::string& t, const TIME::Date& dispo, const TIME::Date& deadline);
-
-};
+    };
 /*************************************** PROJET ******************************************************/
 
 class Projet {
-        typedef std::vector<Tache*> contTache;
     private:
     std::string identificateur;
     std::string titre;
     TIME::Date date_dispo;
     TIME::Date echeance;
-    contTache taches_projet;
+    typedef std::vector<Tache*> contTache;
+    contTache taches_projet; //contient les taches appartenant au projet
     public:
+    friend class ProjetManager; //ProjetManager utilise le tableau des taches du projet
     virtual ~Projet(){}
     Projet(const std::string& id, const std::string& t,const TIME::Date& d, const TIME::Date e): identificateur(id), titre(t), date_dispo(d), echeance(e){  taches_projet.reserve(10);}
-    Projet& ajouterTacheP(Tache& t ){
-        taches_projet.push_back(&t);
-        return *this;
-    }
-    friend class ProjetManager;
+    Projet& ajouterTacheP(Tache& t ); //ajoute la tache au projet
     std::string getId() const { return identificateur; }
     void setId(const std::string& str);
 	std::string getTitre() const { return titre; }
@@ -232,11 +209,6 @@ class Projet {
         if (e<disp) throw CalendarException("erreur Projet : date echeance < date disponibilite");
         date_dispo=disp; echeance=e;
     }
-
-    /*void afficher(std::ostream& fout= std::cout) const {
-        Tache::afficher(fout);
-        }*/
 };
-
 
 #endif
